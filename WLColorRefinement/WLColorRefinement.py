deac4f8b991c89281496cbbce368b4be61697880
Kadjel <kadjel.ramkisoen@gmail.com>
2021-03-31 23:03:12 +0200

    Add function for super_node and super_edge
    Functions to create the super_nodes, the super_edge and to calculate the weight of the super_edge.

--- WLColorRefinement/WLColorRefinement.py
+++ WLColorRefinement/WLColorRefinement.py
@@ -30,25 +30,21 @@ def _update_colors(G):
   return list(map(_to_color, G.ndata.pop('color').cpu().numpy()))
 
 
-def is_possibly_isomorphic(G, H, max_iter=10):
+def wl_coloring(G, max_iter=10):
   """Check if the two given graphs are possibly isomorphic by the 1-Weisfeiler-Lehman algorithm.
 
   Arguments:
       G {networkx.classes.graph.Graph} -- Graph
-      H {networkx.classes.graph.Graph} -- Graph
-
+   
   Keyword Arguments:
       max_iter {int} -- The max number of iterations(default: {10})
 
   Returns:
-      bool -- Return "False" if definitely not isomorphic
+      G {networkx.classes.graph.Graph} -- Graph with attribute color added
   """
-#   G = dgl.from_networkx(G)
-#   H = dgl.from_networkx(H)
 
   # Set initial colors
-  G.ndata['color'] = th.zeros(G.number_of_nodes())
-  H.ndata['color'] = th.zeros(H.number_of_nodes())
+  G.ndata['color'] = th.ones(G.number_of_nodes())
   N = G.number_of_dst_nodes()
   current_max_color = 0
   
@@ -56,24 +52,16 @@ def is_possibly_isomorphic(G, H, max_iter=10):
   for i in range(max_iter):
     #print(i)
     G_colors = _update_colors(G)
-    H_colors = _update_colors(H)
 
     G_unique_colors, G_counts = np.unique(G_colors, return_counts=True)
-    H_unique_colors, H_counts = np.unique(H_colors, return_counts=True)
     G_multiset = dict(zip(G_unique_colors, G_counts))
-    H_multiset = dict(zip(H_unique_colors, H_counts))
-
-#     if G_multiset != H_multiset:
-#       return False
 
     # Recoloring (str -> int)
-    unique_colors = np.unique(np.append(G_unique_colors, H_unique_colors))
+    unique_colors = np.unique(G_unique_colors)
     recolor_map = {color: i + 1 for i, color in enumerate(unique_colors)}
 
     G.ndata['color'] = th.from_numpy(np.array([recolor_map[color]
                                  for color in G_colors]) + current_max_color)
-    H.ndata['color'] = th.from_numpy(np.array([recolor_map[color]
-                                 for color in H_colors]) + current_max_color)
     current_max_color += len(unique_colors)
   
   return True
